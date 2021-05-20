f1da1af2626e3e0e55be3b90605245d519a13789
dimas-avila <67052002+dimas-avila@users.noreply.github.com>
2021-05-20 18:14:13 +0200

    Add files via upload

---
+++ YoNunca/constants/colors.py
@@ -0,0 +1,17 @@
+BACKGROUND = "#121212"
+TEXT = "#84C9FB"
+COMPONENT = "#363636"
+FONT = ("Arial", 16)
+
+
+STYLE = {
+    "font": FONT,
+    "bg": COMPONENT,
+    "fg": TEXT 
+}
+
+MODES = {
+    "NORMAL": "Normal",
+    "ATREVIDO": "Atrevido",
+    "TIRULNA": "Tirulina"
+}
